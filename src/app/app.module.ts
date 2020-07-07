1c2b66870d6908a05c6ce91bad9039bb5aeaa568
Duc <vanduc2514@gmail.com>
2020-07-07 14:27:53 +0700

    Show comment, Place comment onto Post

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,19 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
-import { PostsComponent } from './posts/posts.component';
-import { CommentComponent } from './comment/comment.component';
-import { PostComponent } from './post/post.component';
+import {AppComponent} from './app.component';
+import {PostsComponent} from './posts/posts.component';
+import {CommentComponent} from './comment/comment.component';
+import {PostComponent} from './post/post.component';
+import {CommentsComponent} from './comments/comments.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PostsComponent,
     CommentComponent,
-    PostComponent
+    PostComponent,
+    CommentsComponent
   ],
   imports: [
     BrowserModule
--- src/app/post/post.component.ts
+++ src/app/post/post.component.ts
@@ -1,4 +1,5 @@
 import {Component, Input, OnInit} from '@angular/core';
+import {IPost} from '../ipost';
 
 @Component({
   selector: 'app-post',
@@ -7,7 +8,7 @@ import {Component, Input, OnInit} from '@angular/core';
 })
 export class PostComponent implements OnInit {
   @Input()
-  post: any;
+  post: IPost;
 
   constructor() { }
 

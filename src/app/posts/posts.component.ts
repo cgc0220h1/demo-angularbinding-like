cac872c73b5f67ca252df60c5b62c3ca3e719c3f
Duc <vanduc2514@gmail.com>
2020-07-07 14:23:41 +0700

    Extract class IPost --- Định rõ kiểu cho Post

--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from '@angular/core';
+import {IPost} from '../ipost';
 
 @Component({
   selector: 'app-posts',
@@ -6,16 +7,18 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./posts.component.css']
 })
 export class PostsComponent implements OnInit {
-  posts = [
+  posts: IPost[] = [
     {
       id: 0,
       title: 'Hello World',
-      content: 'Today is a good day'
+      content: 'Today is a good day',
+      comments: [1, 2, 3]
     },
     {
       id: 1,
       title: 'Hello World',
-      content: 'Today is a good day'
+      content: 'Today is a good day',
+      comments: [4, 5, 6]
     }
   ];
 
